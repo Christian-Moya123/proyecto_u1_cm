6187d7f085efbe4f07fec045717fa6093b0d4d47
Christian Moya <cmoyacharro@gamil.com>
2022-05-27 20:32:21 -0500

    Mi primera subida desde consola

--- src/main/java/com/uce/edu/demo/consultorio/CitaMedica.java
+++ src/main/java/com/uce/edu/demo/consultorio/CitaMedica.java
@@ -1,5 +1,10 @@
 package com.uce.edu.demo.consultorio;
 
 public class CitaMedica {
-
+	
+	private Doctor doctor;
+	private Paciente paciente;
+	
+	
+	//https://github.com/Christian-Moya123/proyecto_u1_cm.git
 }
--- src/main/java/com/uce/edu/demo/consultorio/Doctor.java
+++ src/main/java/com/uce/edu/demo/consultorio/Doctor.java
@@ -1,5 +1,23 @@
 package com.uce.edu.demo.consultorio;
 
 public class Doctor {
+	private String nombre;
+	private String Apellido;
+	
+	//SET y GET
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	} 
+	
+	
 
 }
